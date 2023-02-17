c09a7f3942e1be2f317415f1a76fb1bf4af839e6
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-17 23:57:33 +0500

    category apis confgured

--- models/categoryModel.mjs
+++ models/categoryModel.mjs
@@ -1,10 +1,12 @@
 import { Schema, model } from 'mongoose'
 
 const categorySchema = new Schema({
-    ip: { type: String, required: true },
-    category: { type: String, required: true },
+    ip: { type: String },
+    title: { type: String, required: true },
+    file: { type: String, required: true },
+    fileName: { type: String, required: true }, 
     isDeleted: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now }
 })
-export default model('productCategory', categorySchema)
+export default model('category', categorySchema)
 
---
+++ routes/category.mjs
@@ -0,0 +1,82 @@
+import express from "express";
+import Joi from "joi";
+import bucket from "../firebase/index.mjs";
+import auth from "../middlewares/auth.mjs";
+import upload from "../middlewares/multerConfig.mjs";
+import categoryModel from "../models/categoryModel.mjs";
+import fs from 'fs';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const category = await categoryModel.find({}, {}, {
+            sort: { 'title': 1 },
+        })
+        res.status(200).send({
+            message: 'category fetched successfully',
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'something went wrong' })
+    }
+})
+
+router.post('/', [auth , upload.any()], async (req, res) => {
+    const schema = Joi.object({
+        title: Joi.string().required(),
+    })
+    if (!req.verifiedToken.isAdmin) {
+        res.status(401).send({
+            message: 'Unauthorized access'
+        })
+        return
+    }
+    try {
+        const { title } = await schema.validateAsync(req.body)
+        bucket.upload(
+            req.files[0].path,
+            {
+                destination: `saylaniStore/category/${req.files[0].filename}`,
+            },
+            function (err, file, apiResponse) {
+                if (!err) {
+                    file.getSignedUrl({
+                        action: 'read',
+                        expires: '03-09-2999'
+                    }).then(async (urlData, err) => {
+                        if (!err) {
+                            // console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
+                            try {
+                                fs.unlinkSync(req.files[0].path)
+                                //file removed
+                            } catch (err) {
+                                console.error(err)
+                            }
+                            // console.log(urlData);
+                            const postedCategory = await categoryModel.create({
+                                title,
+                                file: urlData[0],
+                                fileName: req.files[0].filename,
+                                ip: req.ip
+                            })
+                            res.status(200).send({
+                                message: 'category fetched successfully',
+                                category: postedCategory
+                            })
+                        }
+                    })
+                } else {
+                    console.log("err: ", err)
+                    res.status(500).send();
+                }
+            });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'something went wrong' })
+    }
+})
+
+export default router
