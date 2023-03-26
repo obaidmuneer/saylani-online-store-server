bba117658c6eb9fa69871358a14bb73ddca22088
obaidmuneer <obaidmuneer55@gmail.com>
2023-03-26 22:34:24 +0500

    context handled

--- routes/chatbot.mjs
+++ routes/chatbot.mjs
@@ -8,36 +8,39 @@ const router = Router()
 
 router.post('/', async (req, res) => {
     const body = req.body
+    // console.log(body);
     const intentName = body.queryResult.intent.displayName
     const params = body.queryResult.parameters
+    console.log(params);
     const session = body.session.split('/')
     const userId = session[session.length - 1]
     // const userId = body.originalDetectIntentRequest.payload.userId
     // console.log(userId);
     // console.log(body);
-    // console.log(intentName);
+    console.log(intentName);
     // console.log(params);
-    const context = body.queryResult.outputContexts
-    // console.log(body.responseId);
+    const contexts = body.queryResult.outputContexts
+    let item, qty;
+    const itemPrice = contexts.find(context => context.name.includes('item_price'));
+    if (itemPrice) {
+        item = itemPrice.parameters.product
+        qty = itemPrice.parameters.quantity
+    }
 
-    const response = (msg) => {
-        return res.send({
-            "fulfillmentMessages": [
-                {
-                    "text": {
-                        "text": [
-                            msg
-                        ]
-                    }
-                }
-            ]
-        })
+    let resposeData;
+    const response = (msg, oc,) => {
+        resposeData = {
+            "fulfillmentMessages": [{ "text": { "text": [msg] } }],
+            outputContexts: oc && contexts.map(context => context.name.includes(oc.name) ?
+                { ...context, lifespanCount: oc.span } : context)
+        }
     }
 
     try {
-        const product = (params.item || context[1]?.parameters?.item) ? await productModel.findOne({
+        const productName = params.item || params.product || item
+        const product = (productName) ? await productModel.findOne({
             title: {
-                $regex: params.item || context[1].parameters.item,
+                $regex: `${productName}`,
                 $options: 'i'
             }
         }) : null
@@ -53,15 +56,21 @@ router.post('/', async (req, res) => {
                     $options: 'i'
                 }
             })
-            response(`currently we have ${products.map(product => ' ' + product.title)} which product do you want to order ?`)
+            response(`we have ${products.map(product => ' ' + product.title)}, what do you want to order?`)
         }
-        if (intentName === 'orderItem') {
-            if (!params.qty && product)
-                response(`we have ${product.title} in ${product.unit_name} how many ${product.unit_name} do you want ?`)
-            else if (params.qty) {
-                if (params.item) {
-                    await create_update_cart(product._id, params.qty, userId)
-                    response(`${params.qty} ${product.unit_name} ${params.item} has been added to cart, do you want to order other items?`)
+        if (intentName === 'orderItem' || intentName === 'itemPrice - order') {
+            const quantity = params.qty || qty
+            if (!quantity && product)
+                response(`we have ${product.title} in ${product.unit_name} how many ${product.unit_name} do you want ?`,
+                    intentName === 'itemPrice - order' && { name: 'item_price', span: 2 })
+            else if (quantity) {
+                if (product) {
+                    await create_update_cart(product._id, quantity, userId)
+                    response(`${quantity} ${product.unit_name.toLowerCase()} of ${product.title} has been added to cart, do you want to order other items?`, {
+                        product: productName,
+                        quantity: quantity
+                    },
+                    )
                 }
                 else response(`please tell me which product do you want to order`)
             }
@@ -70,21 +79,23 @@ router.post('/', async (req, res) => {
             response(`Do you want to checkout?`)
         }
         if (intentName === 'updateItem') {
+            const quantity = params.qty || qty
+            if (!product) return response(`please tell me which item do you want to update`)
+            if (!quantity) return response(`how many units you want to update`)
+            if (!params.intent) return response(`please tell me do want to remove item or add more items`)
             try {
                 await update_cart(userId, product._id, params.qty, params.intent)
-                response(`${params.qty} ${product.unit_name} of ${product.title} has been ${params.intent}d from cart`)
+                response(`${params.qty} ${product.unit_name} of ${product.title} has been ${params.intent === 'increase' ? 'added' : 'removed'} ${params.intent === 'increase' ? 'in' : 'from'} cart`, { name: 'item_price', span: 0 })
             } catch (error) {
                 response(`${error}`)
             }
-
         }
         if (intentName === 'itemPrice') {
-            if (product)
-                !params.qty ?
-                    response(`the price of the ${product.title} is ${product.unit_price} in ${product.unit_name}`) :
-                    response(`the price of the ${params.qty} ${product.title} is ${product.unit_price * params.qty} rs and 1 ${product.title} is  ${product.unit_price} in ${product.unit_name}`)
-            else
-                response(`this product is not available`)
+            if (product) {
+                !params.quantity ?
+                    response(`${product.title} is ${product.unit_price} rs per ${product.unit_name.toLowerCase()}`) :
+                    response(`${params.quantity} ${product.title} is ${product.unit_price * params.quantity} rs and 1 ${product.title} is  ${product.unit_price} rs per ${product.unit_name}`)
+            } else response(`this product is not available`)
         }
         if (intentName === 'checkout') {
             // console.log(context[1].parameters);
@@ -96,6 +107,7 @@ router.post('/', async (req, res) => {
         console.log(error);
         response('something went wrong')
     }
+    res.send(resposeData)
 
 });
 
